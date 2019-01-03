beaabf9402f1601e76ee7cc075d77121a8ad5c04
Jonah Leibowitz <jleibowitz@ucvts.org>
2019-01-03 13:04:21 -0500

    a whole calculator thing at once

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,9 +1,72 @@
+let value1=false;
+let value2=false;
+let value3=false;
+let value4=false;
+let value5=false;
+let value6=false;
+let value7=false;
+let value8=false;
+let value9=false;
+
+for(i=1; i<10; i++){
+  
+}
+
+/////NUMBERS/////
 function button1() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>1</span>";
+}
+function button2() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>2</span>";
+}
+function button3() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>3</span>";
+}
+function button4() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>4</span>";
+}
+function button5() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>5</span>";
+}
+function button6() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>6</span>";
+}
+function button7() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>7</span>";
+}
+function button8() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>8</span>";
+}
+function button9() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>9</span>";
+}
+function button0() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>0</span>";
+}
+///////////////
 
-  const canvas = document.getElementById('canvas1');
-  const ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  ctx.font = "48px Arial";
-  ctx.strokeText("Hello, World!",10,50);
+////FUNCTIONS//////
+function buttonAC() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'> </span>";
+}
+function button2() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>2</span>";
+}
+function button3() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>3</span>";
+}
+function button4() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>4</span>";
+}
+function button5() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>5</span>";
+}
+function button6() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>6</span>";
+}
+function button7() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>7</span>";
+}
+function button8() {
+  document.getElementById("calc-output").innerHTML="<span style='color: white;'>8</span>";
 }
