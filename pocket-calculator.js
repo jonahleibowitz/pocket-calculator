1cc9878cd865566b28ae3709015e709d5ef788ed
Jonah Leibowitz <jleibowitz@ucvts.org>
2019-01-07 13:23:35 -0500

    making the numbers work

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,77 +1,51 @@
-function calculator(){
-let value1;
-let value2;
-let value3;
-let value4;
-let value5;
-let value6;
-let value7;
-let value8;
-let value9;
+//function calculator(){
+//let value1;
+//let value2;
+//let value3;
+//let value4;
+//let value5;
+//let value6;
+//let value7;
+//let value8;
+//let value9;
 
-value1=1;
-value2=3;
-value4=7;
-let values= [value1, value2, value3, value4, value5, value6, value7, value8, value9]
-console.log("test")
-  document.getElementById("calc-output").innerHTML=`<span style='color: white;'>${values.join("")}</span>`;
-}
+//console.log("test")
+//  document.getElementById("calc-output").innerHTML=`<span style='color: white;'>${values.join("")}</span>`;
+//}
+
+var values= []
+
 
 /////NUMBERS/////
 function button1() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>1</span>";
+  if (values.length<9){
+    values.push(1);
+  }
+  document.getElementById("calc-output").innerHTML=`<span style='color: white;'>${values.join("")}</span>`;
 }
+
 function button2() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>2</span>";
+  if (values.length<9){
+    values.push(2);
+  }
+  document.getElementById("calc-output").innerHTML=`<span style='color: white;'>${values.join("")}</span>`;
 }
+
 function button3() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>3</span>";
-}
-function button4() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>4</span>";
-}
-function button5() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>5</span>";
-}
-function button6() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>6</span>";
-}
-function button7() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>7</span>";
-}
-function button8() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>8</span>";
-}
-function button9() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>9</span>";
-}
-function button0() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>0</span>";
+  if (values.length<9){
+    values.push(3);
+  }
+  document.getElementById("calc-output").innerHTML=`<span style='color: white;'>${values.join("")}</span>`;
 }
 ///////////////
 
 ////FUNCTIONS//////
 function buttonAC() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'> </span>";
+  values.length=0;
+  ;
+  document.getElementById("calc-output").innerHTML=`<span style='color: white;'>0</span>`;
 }
-function button2() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>2</span>";
-}
-function button3() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>3</span>";
-}
-function button4() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>4</span>";
-}
-function button5() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>5</span>";
-}
-function button6() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>6</span>";
-}
-function button7() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>7</span>";
-}
-function button8() {
-  document.getElementById("calc-output").innerHTML="<span style='color: white;'>8</span>";
-}
+
+//function button2() {
+//  document.getElementById("calc-output").innerHTML="<span style='color: white;'>2</span>";
+//}
